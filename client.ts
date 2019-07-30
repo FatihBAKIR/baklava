694e0b72ec093f452b326a96266da5bad378d939
Fatih Bakir <mfatihbakir@gmail.com>
2019-07-29 21:46:53 -0700

    cleaned up a bit

--- client.ts
+++ client.ts
@@ -2,7 +2,6 @@ import * as msgpack from "msgpack5";
 import * as fs from "fs";
 import { createHash } from "crypto";
 import bignum = require("bignum");
-import struct = require("python-struct");
 
 function sign_seq(seq: Uint8Array) : Uint8Array
 {
@@ -24,34 +23,36 @@ function merge_signs(base: Uint8Array, add: Uint8Array) : Uint8Array
     return createHash("sha256").update(res).digest();
 }
 
-const seq = (+new Date()).toString();
+function GeneratePutRequest(capability : Buffer, data : Uint8Array) : Buffer {
+    const seq = (+new Date()).toString();
+    const b = new bignum(seq);
+    
+    const sig = capability.slice(capability.length - 32);
 
-const b = new bignum(seq);
+    const seq_hash = sign_seq(b.toBuffer({
+        endian: "little",
+        size: 8
+    }));
 
-const cap = fs.readFileSync("/home/fatih/woof1.cap");
+    const PutId = 1;
 
-const sig = cap.slice(cap.length - 32);
+    const HostId = 5; // Only used for repair
+    const HostSeq = 42; // Only used for repair
 
-const seq_hash = sign_seq(b.toBuffer({
-    endian: "little",
-    size: 8
-}));
+    const body = msgpack().encode(
+        [1, "test", "fwd4_fun", HostId, HostSeq, data]);
+        
+    const req_hash = sign_seq(body.slice(0));
 
-const x = new Date();
-const body = msgpack().encode([1, "test", "fwd4_fun", 5, 10, 
-                                struct.pack("<QQQQQQ", 1, 2, 3, 
-                                    (x.getTime() + x.getTimezoneOffset()*60*1000), 5, 6)]);
+    const merged_sign = merge_signs(merge_signs(sig, seq_hash), req_hash);
 
-const req_hash = sign_seq(body.slice(0));
+    Object.assign(sig, merged_sign);
+    const req = msgpack().encode([cap, body, parseInt(seq)]);
+    return req.slice(0);
+};
 
-const merged_sign = merge_signs(merge_signs(sig, seq_hash), req_hash);
+const cap = fs.readFileSync("woof1.cap");
 
-console.error("signature: " + sig.toString("hex"));
-console.error("seq sig: " + new Buffer(seq_hash).toString("hex"));
-console.error("merged signature: " + new Buffer(merged_sign).toString("hex"));
-Object.assign(sig, merged_sign);
-console.error("signature: " + sig.toString("hex"));
-
-const req = msgpack().encode([cap, body, parseInt(seq)]);
+const req = GeneratePutRequest(cap, new Uint8Array(4));
 
 console.log(req.toString("hex"));
